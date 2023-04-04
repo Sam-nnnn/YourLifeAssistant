347724eec4e0975a57c285e3dd394bb7822ce94d
Sam He <wwgod020590@gmail.com>
2023-04-04 17:32:06 +0800

    init

---
+++ app.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+const { Configuration, OpenAIApi } = require('openai');
+
+//openai instance
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+const app = express();
+
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  const echo = {
+    type: 'text',
+    text: '已加入事件排程',
+  };
+
+  if (echo) {
+    // client.pushMessage(event.replyToken, echo);
+    return client.replyMessage(event.replyToken, echo);
+  }
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
